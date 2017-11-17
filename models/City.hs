17757b8ab66808912be197aab2b826ae5f3bb030
Sam <sam@celery>
2017-11-17 01:37:26 +0000

    all cities (hopefully) added

--- models/City.hs
+++ models/City.hs
@@ -27,73 +27,73 @@ atlanta       =
 baghdad       =
   City ("Baghdad", [cairo, istanbul, tehran, karachi, riyadh], Black, [], False)
 bangkok       =
-  City ("Bangkok", [], [], False)
+  City ("Bangkok", [kolkata, chennai, jakarta, ho_chi_minh, hong_kong], Red, [], False)
 beijing       =
-  City ("Beijing", [], [], False)
+  City ("Beijing", [shanghai, seoul], Red, [], False)
 bogota        =
   City ("Bogota", [miami, mexico_city, lima, buenos_aires, sao_paulo], Yellow, [], False)
 buenos_aires  =
   City ("Buenos Aires", [sau_paulo, bogota], Yellow, [], False)
 cairo         =
-  City ("Cairo", [], [], False)
+  City ("Cairo", [algiers, istanbul, baghdad, riyadh, khartoum], Black, [], False)
 chennai       =
-  City ("Chennai", [], [], False)
+  City ("Chennai", [mumbai, delhi, kolkata, bangkok, jakarta], Black, [], False)
 chicago       =
   City ("Chicago", [san_francisco, montreal, atlanta, los_angeles, mexico_city], Blue, [], False)
 delhi         =
-  City ("Delhi", [], [], False)
+  City ("Delhi", [tehran, karachi, mumbai, chennai, kolkata], Black, [], False)
 essen         =
-  City ("Essen", [], [], False)
-ho_chi_min    =
-  City ("Ho Chi Min City", [], [], False)
+  City ("Essen", [st_petersburg, milan, paris, london], Blue, [], False)
+ho_chi_minh   =
+  City ("Ho Chi Minh City", [jakarta, bangkok, hong_kong, manila], Red, [], False)
 hong_kong     =
-  City ("Hong Kong", [], [], False)
+  City ("Hong Kong", [kolkata, shanghai, taipei, manila, ho_chi_minh, bangkok], Red, [], False)
 istanbul      =
-  City ("Istanbul", [], [], False)
+  City ("Istanbul", [milan, st_petersburg, moscow, baghdad, cairo, algiers], Black, [], False)
 jakarta       =
-  City ("Jakarta", [], [], False)
+  City ("Jakarta", [chennai, bangkok, ho_chi_minh, sydney], Red, [], False)
 johannesburg  =
-  City ("Johannesburg", [], [], False)
+  City ("Johannesburg", [kinshasa, khartoum], Yellow, [], False)
 karachi       =
-  City ("Karachi", [], [], False)
+  City ("Karachi", [riyadh, baghdad, tehran, delhi, mumbai], Black, [], False)
 khartoum      =
-  City ("Khartoum", [], [], False)
+  City ("Khartoum", [johannesburg, kinshasa, lagos, cairo], Yellow, [], False)
 kinshasa      =
-  City ("Kinshasa", [], [], False)
+  City ("Kinshasa", [lagos, johannesburg, khartoum], Yellow, [], False)
 kolkata       =
-  City ("Kolkata", [], [], False)
+  City ("Kolkata", [delhi, chennai, bangkok, hong_kong], Black, [], False)
 lagos         =
-  City ("Lagos", [], [], False)
+  City ("Lagos", [khartoum, kinshasa, sao_paulo], Yellow, [], False)
 lima          =
   City ("Lima", [santiago, bogota, mexico_city], Yellow, [], False)
 london        =
-  City ("London", [], [], False)
+  City ("London", [essen, paris, madrid, new_york], Blue, [], False)
 los_angeles   =
   City ("Los Angeles", [san_francisco, chicago, mexico_city, sydney], Yellow, [], False)
 madrid        =
-  City ("Madrid", [], [], False)
+  City ("Madrid", [new_york, london, paris, algiers, sao_paulo], Blue, [], False)
 manila        =
-  City ("Manila", [], [], False)
+  City ("Manila", [ho_chi_minh, hong_kong, taipei, san_francisco, sydney], Red, [], False)
 mexico_city   =
   City ("Mexico City", [los_angeles, chicago, miami, bogota, lima], Yellow, [], False)
 miami         =
   City ("Miami", [mexico_city, bogota, atlanta, washington], Yellow, [], False)
 milan         =
-  City ("Milan", [], [], False)
+  City ("Milan", [istanbul, essen, paris], Blue, [], False)
 montreal      =
   City ("Montreal", [chicago, new_york, washington], Blue, [], False)
 moscow        =
-  City ("Moscow", [], [], False)
+  City ("Moscow", [st_petersburg, istanbul, tehran], Black, [], False)
 mumbai        =
-  City ("Mumbai", [], [], False)
+  City ("Mumbai", [karachi, delhi, chennai], Black, [], False)
 new_york      =
   City ("New York", [montreal, washington, london, madrid], Blue, [], False)
 osaka         =
-  City ("Osaka", [], [], False)
+  City ("Osaka", [tokyo, taipei], Red, [], False)
 paris         =
-  City ("Paris", [], [], False)
+  City ("Paris", [london, madrid, algiers, milan, essen], Blue, [], False)
 riyadh        =
-  City ("Riyadh", [], [], False)
+  City ("Riyadh", [cairo, baghdad, karachi], Black, [], False)
 san_francisco =
   City ("San Francisco", [los_angeles, chicago, tokyo, manila], Blue, [], False)
 santiago      =
@@ -101,18 +101,18 @@ santiago      =
 sao_paulo     =
   City ("Sao Paulo", [buenos_aires, bogota, lagos, madrid], Yellow, [], False)
 seoul         =
-  City ("Seoul", [], [], False)
+  City ("Seoul", [beijing, shanghai, tokyo], Red, [], False)
 shanghai      =
-  City ("Shanghai", [], [], False)
+  City ("Shanghai", [beijing, seoul, tokyo, taipei, hong_kong], Red, [], False)
 st_petersburg =
-  City ("St. Petersburg", [], [], False)
+  City ("St. Petersburg", [essen, istanbul, moscow], Blue, [], False)
 sydney        =
-  City ("Sydney", [], [], False)
+  City ("Sydney", [jakarta, manila, los_angeles], Red, [], False)
 taipei        =
-  City ("Taipei", [], [], False)
+  City ("Taipei", [osaka, manila, hong_kong, shanghai], Red, [], False)
 tehran        =
-  City ("Tehran", [], [], False)
+  City ("Tehran", [moscow, baghdad, karachi, delhi], Black, [], False)
 tokyo         =
-  City ("Tokyo", [], [], False)
+  City ("Tokyo", [seoul, osaka, shanghai, san_francisco], Red, [], False)
 washington    =
   City ("Washington", [miami, atlanta, montreal, new_york], Blue, [], False)
